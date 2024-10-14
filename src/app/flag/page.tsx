314ca05d11e2e4f505f77d7adda6564f50881500
Harsh Agrawal <97937899+harshu1611@users.noreply.github.com>
2024-10-14 12:22:49 +0530

    Add Indian flag on /flag route

---
+++ src/app/flag/page.tsx
@@ -0,0 +1,55 @@
+import React from 'react'
+
+export default function page() {
+
+  return (
+    <>
+    <div style={{width:'100%',height:'33.33vh',backgroundColor:'orange'}}></div>
+    <div style={{width:'100%',height:'33.33vh',backgroundColor:'white',display:'flex',justifyItems:'center',alignItems:'center',justifyContent:'center'}}>
+        <div style={{height:'100%', borderRadius:'100%',borderStyle:'solid',borderColor:'blue',borderWidth:'3px',width:'33.33vh',display:'flex',flexDirection:'column',justifyItems:'center',justifyContent:'center',alignItems:'center'}}>
+            <div style={{width:'3px',height:'33.33vh',backgroundColor:'blue',position:'absolute'}}>
+                .
+            </div>
+            <div style={{width:'3px',height:'33.33vh',backgroundColor:'blue',rotate:'15deg',position:'absolute'}}>
+                .
+            </div>
+            <div style={{width:'3px',height:'33.33vh',backgroundColor:'blue',rotate:'30deg',position:'absolute'}}>
+                .
+            </div>
+            <div style={{width:'3px',height:'33.33vh',backgroundColor:'blue',rotate:'45deg',position:'absolute'}}>
+                .
+            </div>
+            <div style={{width:'3px',height:'33.33vh',backgroundColor:'blue',rotate:'60deg',position:'absolute'}}>
+                .
+                </div>
+                <div style={{width:'3px',height:'33.33vh',backgroundColor:'blue',rotate:'75deg',position:'absolute'}}>
+                .
+                </div> 
+                <div style={{width:'3px',height:'33.33vh',backgroundColor:'blue',rotate:'90deg',position:'absolute'}}>
+                .
+                </div>
+                <div style={{width:'3px',height:'33.33vh',backgroundColor:'blue',rotate:'105deg',position:'absolute'}}>
+                .
+                </div>
+                <div style={{width:'3px',height:'33.33vh',backgroundColor:'blue',rotate:'120deg',position:'absolute'}}>
+                .
+                </div>
+                <div style={{width:'3px',height:'33.33vh',backgroundColor:'blue',rotate:'135deg',position:'absolute'}}>
+                .
+                </div>
+                <div style={{width:'3px',height:'33.33vh',backgroundColor:'blue',rotate:'150deg',position:'absolute'}}>
+                .
+                </div>
+                <div style={{width:'3px',height:'33.33vh',backgroundColor:'blue',rotate:'165deg',position:'absolute'}}>
+                .
+                </div>
+                <div style={{width:'3px',height:'33.33vh',backgroundColor:'blue',position:'absolute'}}>
+                .
+                </div>
+        </div>
+    </div>
+    <div style={{width:'100%',height:'33.33vh',backgroundColor:'green'}}></div>
+    </>
+    
+  )
+}
